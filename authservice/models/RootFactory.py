5926a1a075b7c0afbc8f609c62ef6173fa5737e9
Steven Locke <steve.m.locke@gmail.com>
2016-09-01 21:54:45 -0400

    bypass groups and use authenticated for permissions for now

--- authservice/models/RootFactory.py
+++ authservice/models/RootFactory.py
@@ -5,10 +5,14 @@ from pyramid.security import (
     )
 
 
+# TODO: get groups working with pyramid_jwt
 class RootFactory(object):
+    """currently for the sake of the api I will be using Authenticated for permission...
+    this lacks fine-grain control and must be changed"""
     __acl__ = [
         (Allow, 'g:super_admin', ALL_PERMISSIONS),
         (Allow, 'g:admin', 'admin'),
+        (Allow, Authenticated, ('admin',)),
     ]
 
     def __init__(self, request):
